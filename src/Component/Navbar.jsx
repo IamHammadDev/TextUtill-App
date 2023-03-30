f64c93ffba38945031a75345f742156644b1a413
HAMMAD <108028597+HAMMAD-DEVELOPER-TECH@users.noreply.github.com>
2023-03-30 12:06:37 +0500

    Using useLocation Hook

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = (props) => {
+  let location = useLocation();
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location]);
+
   return (
     <>
       <nav
@@ -26,12 +31,16 @@ const Navbar = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">
+                <Link className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`} aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <Link className={`nav-link ${
+                    location.pathname === "/about" ? "active" : ""
+                  }`} to="/about">
                   {props.aboutText}
                 </Link>
               </li>
